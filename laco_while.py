78ced99256c3515e60ffcb930dfe828f6f8a2795
Barto <alheirosb@gmail.com>
2026-01-30 16:33:05 -0300

    laco while

---
+++ laco_while.py
@@ -0,0 +1,6 @@
+palavra = input('Entre com uma palavra com o a letra X ')
+
+while 'x' not in palavra:
+    palavra = input('Entre com uma palavra com a letra X ')
+
+print('Finalmente, você inseriu uma palavra válida com a letra X')
