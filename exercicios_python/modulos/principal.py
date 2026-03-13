38c62c5892ae43db2e188892a584e0325d6eda8c
Barto <alheirosb@gmail.com>
2026-03-13 16:54:14 -0300

    att modulos

--- exercicios_python/modulos/principal.py
+++ exercicios_python/modulos/principal.py
@@ -1,7 +1,10 @@
-import imc
+import imc as c
+from imc import classifica_imc as c_imc
 
 peso = 70
 altura = 1.8
 
-resultado = imc.calcula_imc(90, 1.5)
-print(resultado)
+indice = c.calcula_imc(90, 1.5)
+print(indice)
+classifica_imc = c_imc(indice)
+print(classifica_imc)
