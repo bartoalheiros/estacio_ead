bf9ae93c9d885fd9171ce12839bb900495469961
Barto <alheirosb@gmail.com>
2026-03-19 16:50:53 -0300

    tkinter botao

--- exercicios_python/gui/teste_tk.py
+++ exercicios_python/gui/teste_tk.py
@@ -1,3 +1,12 @@
 import tkinter as tk
 
-tk._test()
+janela = tk.Tk()
+
+janela.title("Minha Aplicação")
+janela.geometry("300x200")
+
+# Criando um botão
+botao = tk.Button(janela, text="Clique Aqui") #Posicionando
+botao.place(x=50, y=50)
+
+janela.mainloop()
