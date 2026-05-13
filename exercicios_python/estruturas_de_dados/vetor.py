9afd9c793b94a6b8b3005d3cdd0ec9ee1c63b70f
Bartô <alheirosb@gmail.com>
2026-05-13 20:40:06 -0300

    início das aulas de vetores

--- exercicios_python/estruturas_de_dados/vetor.py
+++ exercicios_python/estruturas_de_dados/vetor.py
@@ -3,3 +3,9 @@ from numpy import array
 notas_real = array([2, 5, 10, 20, 50, 100], dtype=int)
 
 print(notas_real[1])
+
+#definir matriz é como definimos o vetor, só que a matriz vai ter várias dimensões
+cadeiras = array([['Português', 'Matemática', 'Química'], ['História', 'Geografia', 'Física']], dtype=str)
+
+print(cadeiras[0][0])
+print(cadeiras[1][0])
