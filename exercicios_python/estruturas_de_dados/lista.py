2dff4731fe8a17b9ac35c43f0f142d38215d2eff
Barto <alheirosb@gmail.com>
2026-05-22 17:00:08 -0300

    multiplicação de matrizes: finalizado e início de listas.

---
+++ exercicios_python/estruturas_de_dados/lista.py
@@ -0,0 +1,8 @@
+minha_lista = [1, 'moto', [1, 2, 3]]
+for elemento in minha_lista:
+    print(elemento)
+
+#verificar porque não funciona.
+lista = []
+for i in range(0, len(lista)):
+    print(lista[i])
