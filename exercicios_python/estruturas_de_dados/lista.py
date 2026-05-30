75ab87a0ce1755d703f4e0bf42561aa039b58417
Bartô <alheirosb@gmail.com>
2026-05-30 11:32:52 -0300

    listas continuacao 2

--- exercicios_python/estruturas_de_dados/lista.py
+++ exercicios_python/estruturas_de_dados/lista.py
@@ -1,10 +1,55 @@
-minha_lista = [1, 'moto', [1, 2, 3]]
-for elemento in minha_lista:
-    print(elemento)
+#mostrando um item específico da lista
+#lista = ['a', 'b', 'c', 'd']
+#item = lista[1]
+#print(item)
 
+# descobrindo o índice de um elemento na lista
+# lista = ['a', 'b', 'c', 'b']
+# indice_c = lista.index('c')
+# print(indice_c)
+#
+# indice_b = lista.index('b')
+# print(indice_b)
+
+# lista = ['avião', 'helicóptero', 'carro', 'navio']
+# indice_carro = lista.index('carro')
+# print(indice_carro)
+
+#Alteração do elemento na posição de índice 2 da lista
+# lista[indice_carro] = 'moto'
+# print(lista)
+
+#percorrendo listas em python
 #verificar porque não funciona.
-lista = []
-for elemento in lista:
-    print(elemento)
-for i in range(0, len(lista)):
-    print(i)
+#lista = ['a', 'b', 'c', 'd']
+#for elemento in lista:
+#    print(elemento)
+
+# minha_lista = [1, 'moto', [1, 2, 3]]
+# for elemento in minha_lista:
+#    print(elemento)
+
+#contando as posições da lista
+# lista = ['a', 'b', 'c', 'd']
+# for i in range(0, len(lista)):
+#    print(i)
+
+# lista = list()
+# lista.append('carro')
+# print(lista)
+# lista.append('moto')
+# lista.append('avião')
+# print(lista)
+# lista.insert(1, 'bicicleta') #insere o elemento bicicleta na posição de índice 1
+# print(lista)
+
+# lista_a = [1, 2, 3]
+# lista_b = [4, 5, 6]
+# lista_a.append(lista_b)
+# print(lista_a)
+#[1, 2, 3, [4, 5, 6]] lista_b foi adicionada dento da lista_a como um elemento único
+# lista_a = [1, 2, 3]
+# lista_a.extend(lista_b)
+# print(lista_a)
+#[1, 2, 3, 4, 5, 6] lista_a e lista_b foram concatenadas
+
