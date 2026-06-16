54e657046d91efc0d6eb6d699c31e76d69942f9d
Bartô <alheirosb@gmail.com>
2026-06-16 19:48:53 -0300

    conjuntos e conjuntos_math

---
+++ exercicios_python/estruturas_de_dados/conjuntos.py
@@ -0,0 +1,21 @@
+# conjunto = set()
+# conjunto.add(1)
+# conjunto.add(2)
+# print(conjunto)
+# #não é possível adicionar o 2 novamente
+# #pois ele já existe no conjunto
+# conjunto.add(2)
+# print(conjunto)
+
+# conjunto = {1, 2, 3}
+# print(conjunto)
+# conjunto.discard(1)
+# conjunto.remove(2)
+# print(conjunto)
+#
+# # conjunto.discard(5)
+# # conjunto.remove(5)
+#
+# conjunto.clear()
+# print(conjunto)
+
---
+++ exercicios_python/estruturas_de_dados/conjuntos_math.py
@@ -0,0 +1,9 @@
+conjunto_a = {1, 2, 3}
+conjunto_b = {4, 5, 6}
+
+print(conjunto_a)
+print(conjunto_b)
+
+uniao = conjunto_a.union(conjunto_b)
+print(uniao)
+
