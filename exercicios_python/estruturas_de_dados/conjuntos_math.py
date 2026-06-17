7b0572acfe8a3886e73150757272c672f7c5c435
Barto <alheirosb@gmail.com>
2026-06-17 16:47:13 -0300

    estruturas de dados finalizado. aulas(exercicios) do material estacio

--- exercicios_python/estruturas_de_dados/conjuntos_math.py
+++ exercicios_python/estruturas_de_dados/conjuntos_math.py
@@ -1,5 +1,6 @@
 conjunto_a = {1, 2, 3}
 conjunto_b = {4, 5, 6}
+conjunto_b = {3, 4, 5}
 
 print(conjunto_a)
 print(conjunto_b)
@@ -7,3 +8,13 @@ print(conjunto_b)
 uniao = conjunto_a.union(conjunto_b)
 print(uniao)
 
+intersecao = conjunto_a.intersection(conjunto_b)
+print(intersecao)
+
+#(a - b)
+diferenca = conjunto_a.difference(conjunto_b)
+print(diferenca)
+
+#(b - a)
+diferenca_2 = conjunto_b.difference(conjunto_a)
+print(diferenca_2)
