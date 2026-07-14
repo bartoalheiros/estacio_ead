0f0e0a78ad09c51a0bb74bc412b3d1665aa4d59c
Barto <alheirosb@gmail.com>
2026-07-14 11:06:59 -0300

    oo metodos com retorno

--- exercicios_python/oo/Conta2.py
+++ exercicios_python/oo/Conta2.py
@@ -5,5 +5,26 @@ class Conta:
         self.nomeTitular = nomeTitular
         self.saldo = saldo
 
+    def depositar(self, valor):
+        self.saldo += valor
+
+    def sacar(self, valor):
+        if self.saldo < valor:
+            return False
+        else:
+            self.saldo -= valor
+            return True
+
+    def gerar_extrato(self):
+        print(f"numero: {self.numero} \ncpf:"
+              f"{self.cpf}\nsaldo: {self.saldo}")
+
 def main():
     c1 = Conta(1, 1, "Joao", 0)  # Objeto sendo criado
+    c1.depositar(300)
+    saque = c1.sacar(400)
+    c1.gerar_extrato()
+    print(f"O saque foi realizado? {saque}")
+
+if __name__ == "__main__":
+    main()
