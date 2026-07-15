e32a3e774041d81552c8ae78fe9309420186c55e
Barto <alheirosb@gmail.com>
2026-07-15 16:37:06 -0300

    Cliente.py criada Conta.py atualizada

--- exercicios_python/oo/banco/Conta_.py
+++ exercicios_python/oo/banco/Conta_.py
@@ -1,5 +1,6 @@
 class Conta_:
-    def __init__(self, numero, cpf, nomeTitular, saldo):
+    def __init__(self, clientes, numero, cpf, nomeTitular, saldo):
+        self.clientes = clientes
         self.numero = numero
         self.cpf = cpf
         self.nomeTitular = nomeTitular
@@ -10,14 +11,13 @@ class Conta_:
 
     def sacar(self, valor):
         if self.saldo < valor:
-            return False
+            return "Não existe saldo suficiente"
         else:
             self.saldo -= valor
-            return True
+            return "Transferência Realizada!"
 
-    def gerar_extrato(self):
-        print(f"numero: {self.numero} \ncpf:"
-              f"{self.cpf}\nsaldo: {self.saldo}")
+    def gerar_saldo(self):
+        print(f"numero: {self.numero} \nsaldo: {self.saldo}")
 
     def transfereValor(self, contaDestino, valor):
         if self.saldo < valor:
@@ -25,13 +25,13 @@ class Conta_:
         else:
             contaDestino.depositar(valor)
             self.saldo -= valor
-            return "Tranferência Realizada"
+            return "Transferência Realizada"
 
 def main():
     c1 = Conta_(1, 1, "Joao", 0)  # Objeto sendo criado
     c1.depositar(300)
     saque = c1.sacar(400)
-    c1.gerar_extrato()
+    c1.gerar_saldo()
     print(f"O saque foi realizado? {saque}")
 
 if __name__ == "__main__":
